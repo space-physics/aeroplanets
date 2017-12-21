9332538db61b067b1ab3ee561c788fb5aa4f398a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-21 13:48:25 -0500

    2to3

--- utils/eigenvals.py
+++ utils/eigenvals.py
@@ -1,16 +1,14 @@
-from scipy.linalg import eigh,eig
-from pylab import *
+import numpy as np
+
 def mat_eigh(mat):
 	""" Print the eigen  value and vectors for a symmetrix matrix """
-	data = matrix(mat)
-	eigval, eigvec = eigh(mat)
-	print "Eigen values ", eigval
-	print "Eigen vectors ", eigval
+	eigval, eigvec = np.linalg.eigh(mat)
+	print("Eigen values ", eigval)
+	print("Eigen vectors ", eigval)
 
 
 def mat_eig(mat):
 	""" Print the eigen  value and vectors for a matrix """
-	data = matrix(mat)
-	eigval, eigvec = eig(mat)
-	print "Eigen values ", eigval
-	print "Eigen vectors ", eigval
+	eigval, eigvec = np.linalg.eig(mat)
+	print("Eigen values ", eigval)
+	print("Eigen vectors ", eigval)
