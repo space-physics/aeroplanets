56d5ff2fcaa8576288960d4e75adaad8e132980b
Michael Hirsch <scivision@users.noreply.github.com>
2021-07-05 17:53:03 -0400

    IntelLLVM compat

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -7,19 +7,18 @@ set(CMAKE_CONFIGURATION_TYPES "Release;RelWithDebInfo;Debug" CACHE STRING "Build
 #endif()
 
 
-if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
+if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
   if(WIN32)
-    add_compile_options(/arch:native)
+    add_compile_options(/QxHost)
     # /heap-arrays is necessary to avoid runtime errors in programs using this library
     string(APPEND CMAKE_Fortran_FLAGS " /heap-arrays")
   else()
-    add_compile_options(-march=native)
+    add_compile_options(-xHost)
   endif()
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   add_compile_options(-mtune=native)
 endif()
 
-unset(gcc10opts)
 if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
   set(gcc10opts -fallow-argument-mismatch)
 endif()
