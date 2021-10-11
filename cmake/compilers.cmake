b18f07e25531111e7b06032e421bd136f938b48f
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-11 00:36:09 -0400

    cmake build type

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -7,14 +7,11 @@ set(CMAKE_CONFIGURATION_TYPES "Release;RelWithDebInfo;Debug" CACHE STRING "Build
 #endif()
 
 
-if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
-  if(WIN32)
-    add_compile_options(/QxHost)
-    # /heap-arrays is necessary to avoid runtime errors in programs using this library
-    string(APPEND CMAKE_Fortran_FLAGS " /heap-arrays")
-  else()
-    add_compile_options(-xHost)
-  endif()
+if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+  add_compile_options(
+    $<$<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>
+    $<$<COMPILE_LANGUAGE:Fortran>:-heap-arrays>
+  )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   add_compile_options(-mtune=native)
 endif()
